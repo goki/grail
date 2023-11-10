effab57fe58bbc9a006e102b93357e3f2e47ccaf
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-09 17:21:00 -0800

    added files

---
+++ grail/auth.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package grail
+
+// AuthGmail authenticates the user with gmail.
+func (a *App) AuthGmail() error {
+	return nil
+}
