ad53403fb31542806bae7d543e9ff58731757c39
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-26 15:43:01 -0800

    updated version to v0.0.12

--- grail/version.go
+++ grail/version.go
@@ -4,9 +4,9 @@ package grail
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.11"
+	Version = "v0.0.12"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "a2181b4"
+	GitCommit = "f2cf6a9"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-21 00:40"
+	VersionDate = "2023-12-26 23:43"
 )
